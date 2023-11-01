d868d8f2f8d0e188aa1f47bf07dcc366062e59c6
knightian <ian@whiteknightit.com.au>
2023-11-02 04:10:08 +1100

    readd initial db migrations

---
+++ RocketRMM/Migrations/LogsDbContext_Migrations/20231101170950_InitialCreate_LogsDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RocketRMM.Migrations.LogsDbContext_Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate_LogsDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "_logEntries",
+                columns: table => new
+                {
+                    RowKey = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Severity = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Message = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    API = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SentAsAlert = table.Column<bool>(type: "tinyint(1)", nullable: true),
+                    Username = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__logEntries", x => x.RowKey);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "_logEntries");
+        }
+    }
+}
