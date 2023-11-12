08be153a870f2815f559d6b80b0ad78fdec26707
Ian Harris - White Knight IT <ian@whiteknightit.com.au>
2023-11-12 16:44:25 +1100

    latest

--- RocketRMM/Utilities.cs
+++ RocketRMM/Utilities.cs
@@ -595,6 +595,8 @@ namespace RocketRMM
                             // Execute update-ca-certificates to install the CA cert into trusted, this will fail if not root
                             Process process = new();
                             process.StartInfo.FileName = "/usr/sbin/update-ca-certificates";
+                            process.StartInfo.UseShellExecute = true;
+                            process.Start();
                             await process.WaitForExitAsync();
 
                             if (process.ExitCode == 0)
